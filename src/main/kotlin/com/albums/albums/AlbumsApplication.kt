64ed12b26a84e2b64ffa9c9a7b3a74480b3a928c
JGMartFern <jgmartfern@gmail.com>
2024-07-03 13:37:24 +0200

    first commit with base project and empty readme file

---
+++ src/main/kotlin/com/albums/albums/AlbumsApplication.kt
@@ -0,0 +1,11 @@
+package com.albums.albums
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AlbumsApplication
+
+fun main(args: Array<String>) {
+	runApplication<AlbumsApplication>(*args)
+}
---
+++ src/test/kotlin/com/albums/albums/AlbumsApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.albums.albums
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AlbumsApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
