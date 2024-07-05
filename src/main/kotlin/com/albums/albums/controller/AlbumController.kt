2f6b3b420c33f39b90374afb0cf379c9023567b8
JGMartFern <jgmartfern@gmail.com>
2024-07-05 16:14:26 +0200

    docs: add openapi annotations for AlbumController

--- src/main/kotlin/com/albums/albums/controller/AlbumController.kt
+++ src/main/kotlin/com/albums/albums/controller/AlbumController.kt
@@ -2,14 +2,17 @@ package com.albums.albums.controller
 
 import com.albums.albums.model.Album
 import com.albums.albums.service.AlbumService
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
+@Tag(name = "Albums", description = "API for managing Albums from JSONPlaceholder")
 @RestController
 @RequestMapping("/api/albums")
 class AlbumController(private val service: AlbumService) {
-
+    @Operation(summary = "It obtains all albums", description = "It returns a collection of all available albums")
     @GetMapping
     fun getAlbums(): Collection<Album> = service.getAlbums()
 }
