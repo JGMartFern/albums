f6b1bd77440a04c508855a4ce4a1abd51c1d8d7f
JGMartFern <jgmartfern@gmail.com>
2024-07-09 18:25:15 +0200

    fix: modify AlbumService for including photos

--- src/main/kotlin/com/albums/albums/service/AlbumService.kt
+++ src/main/kotlin/com/albums/albums/service/AlbumService.kt
@@ -1,11 +1,20 @@
 package com.albums.albums.service
 
 import com.albums.albums.datasource.AlbumDataSource
+import com.albums.albums.datasource.PhotoDataSource
 import com.albums.albums.model.Album
 import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.stereotype.Service
 
 @Service
-class AlbumService(@Qualifier("websiteAlbum") private val dataSource: AlbumDataSource) {
-    fun getAlbums(): Collection<Album> = dataSource.getAlbums()
+class AlbumService(@Qualifier("websiteAlbum") private val albumDataSource: AlbumDataSource, @Qualifier("websitePhoto") private val photoDataSource: PhotoDataSource) {
+    fun getAlbums(): Collection<Album> {
+        val albums = albumDataSource.getAlbums()
+        val photos = photoDataSource.getPhotos()
+
+        return albums.map { album ->
+            val albumPhotos = photos.filter { it.albumId == album.id }
+            album.copy(photos = albumPhotos)
+        }
+    }
 }
--- src/test/kotlin/com/albums/albums/service/AlbumServiceTest.kt
+++ src/test/kotlin/com/albums/albums/service/AlbumServiceTest.kt
@@ -1,19 +1,21 @@
 package com.albums.albums.service
 
 import com.albums.albums.datasource.AlbumDataSource
+import com.albums.albums.datasource.PhotoDataSource
 import io.mockk.mockk
 import io.mockk.verify
 import org.junit.jupiter.api.Test
 
 class AlbumServiceTest {
 
-    private val dataSource: AlbumDataSource = mockk(relaxed = true)
-    private val albumService = AlbumService(dataSource)
+    private val albumDataSource: AlbumDataSource = mockk(relaxed = true)
+    private val photoDataSource: PhotoDataSource = mockk(relaxed = true)
+    private val albumService = AlbumService(albumDataSource, photoDataSource)
 
     @Test
     fun `should call the data source to get the albums`() {
         val albums = albumService.getAlbums()
 
-        verify(exactly = 1) { dataSource.getAlbums() }
+        verify(exactly = 1) { albumDataSource.getAlbums() }
     }
 }
