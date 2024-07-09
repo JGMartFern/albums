36e652197613d26ffd888f38b13377a275c89513
JGMartFern <jgmartfern@gmail.com>
2024-07-09 18:06:42 +0200

    fix: change Album model to include photos

--- src/main/kotlin/com/albums/albums/model/Album.kt
+++ src/main/kotlin/com/albums/albums/model/Album.kt
@@ -3,5 +3,6 @@ package com.albums.albums.model
 data class Album(
     val userId: Int,
     val id: Int,
-    val title: String
+    val title: String,
+    val photos: Collection<Photo> = emptyList()
 )
